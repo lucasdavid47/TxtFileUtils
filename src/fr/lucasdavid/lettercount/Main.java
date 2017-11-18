1989c374642a43827fd2a223d56eb31cf3d07846
Lucas <lucas.david44000@gmail.com>
2017-11-18 09:43:03 +0100

    First Commit
    Ajouter de la fonction pour afficher le nombre de lignes, de mots et de lettres

---
+++ src/fr/lucasdavid/lettercount/Main.java
@@ -0,0 +1,34 @@
+package fr.lucasdavid.lettercount;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		int words = 0;
+		int letters = 0;
+		int lines = 0;
+
+		File file = new File("file path");
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(file);
+
+		while (in.hasNextLine()) {
+			lines++;
+			String line = in.nextLine();
+			letters += line.length();
+			words += new StringTokenizer(line, "  ,").countTokens();
+
+
+
+		}
+		
+		System.out.println("Nombre de lignes: " + lines);
+		System.out.println("Nombre de mots: " + words);
+		System.out.println("Nombre de lettres: " + letters);
+	}
+
+}
